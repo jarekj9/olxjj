e8ad702c87c8fd275dbb31f1177bce8f1343bb88
pi <jerry.esp.sha@gmail.com>
2019-04-20 19:49:54 +0200

    new methods to list all prices for a link

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 from olxjj import OLXJJ
 
-url = "https://www.olx.pl/nieruchomosci/mieszkania/bydgoszcz/?search%5Bfilter_float_price%3Afrom%5D=284900&search%5Bfilter_float_price%3Ato%5D=284900&search%5Bfilter_enum_floor_select%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_select%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_select%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_select%5D%5B3%5D=floor_4&search%5Bfilter_enum_floor_select%5D%5B4%5D=floor_5&search%5Bfilter_enum_floor_select%5D%5B5%5D=floor_6&search%5Bfilter_enum_floor_select%5D%5B6%5D=floor_7&search%5Bfilter_enum_floor_select%5D%5B7%5D=floor_8&search%5Bfilter_enum_floor_select%5D%5B8%5D=floor_9&search%5Bfilter_enum_floor_select%5D%5B9%5D=floor_10&search%5Bfilter_enum_floor_select%5D%5B10%5D=floor_11&search%5Bfilter_enum_builttype%5D%5B0%5D=blok&search%5Bfilter_enum_builttype%5D%5B1%5D=apartamentowiec&search%5Bfilter_float_m%3Afrom%5D=45&search%5Bfilter_float_m%3Ato%5D=60&search%5Bfilter_enum_rooms%5D%5B0%5D=three"	
-words=['internet','telefon']
+url = "https://www.olx.pl/nieruchomosci/mieszkania/bydgoszcz/?search%5Bfilter_float_price%3Afrom%5D=250000&search%5Bfilter_float_price%3Ato%5D=330000&search%5Bfilter_enum_floor_select%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_select%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_select%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_select%5D%5B3%5D=floor_4&search%5Bfilter_enum_floor_select%5D%5B4%5D=floor_5&search%5Bfilter_enum_floor_select%5D%5B5%5D=floor_6&search%5Bfilter_enum_floor_select%5D%5B6%5D=floor_7&search%5Bfilter_enum_floor_select%5D%5B7%5D=floor_8&search%5Bfilter_enum_floor_select%5D%5B8%5D=floor_9&search%5Bfilter_enum_floor_select%5D%5B9%5D=floor_10&search%5Bfilter_enum_floor_select%5D%5B10%5D=floor_11&search%5Bfilter_enum_builttype%5D%5B0%5D=blok&search%5Bfilter_enum_builttype%5D%5B1%5D=apartamentowiec&search%5Bfilter_float_m%3Afrom%5D=45&search%5Bfilter_float_m%3Ato%5D=60&search%5Bfilter_enum_rooms%5D%5B0%5D=three"	
+words=['loggia','logia','winda']
 olx=OLXJJ(url)
 
-print(olx.get_links_with_word("and",words))
+#print(olx.get_links_with_word("and",words))
 print(olx.get_links_with_word("or",words))
 
 	
--- olxjj.py
+++ olxjj.py
@@ -35,19 +35,19 @@ class OLXJJ:
 		self.righttab = self.soup.find("table", {"class": "fixed offers breakword redesigned"}) 
 		self.offers=[]
 		for link in self.righttab('a'):
-			if "promoted" in str(link): continue
+			if "promoted" in str(link): continue	
 			if "oferta" in str(link) and link.get('href') not in self.offers: self.offers.append(link.get('href'))
 		return self.offers
 		
 	#returns offer links for all pages (combined)
 	def _get_all_links(self):
+		self.output=[]
 		for self.page_nr in range(1,self.nr_of_pages+1):
-			self.output=[]
 			if self.url[-1:] is "/": self.r  = requests.get(self.url+"?page="+str(self.page_nr))  #because there are different versions of links
 			else:                    self.r  = requests.get(self.url+"&page="+str(self.page_nr))
 			self.data = self.r.text
 			self.soup = BeautifulSoup(self.data,features="html.parser")
-			for link in self._get_links_from_page(self.soup): self.output.append(link)
+			for self.link in self._get_links_from_page(self.soup): self.output.append(self.link)
 			self.progress.run()
 		return self.output
 		
@@ -68,18 +68,78 @@ class OLXJJ:
 			self.r  = requests.get(self.link)
 			self.data = self.r.text
 			self.soup = BeautifulSoup(self.data,features="html.parser")
-			self.description = self.soup.find(True, {"class": "section-description"})  #search in description field of olx or otodom
-			if(self.description is None): self.description = self.soup.find(True, {"class": "clr lheight20 large"})
+			
+			#get description text
+			self.description=""
+			try: 
+				self.description=self.soup.find("section", {"class": "section-description"}).find("div")   #for otodom
+			except: 
+				pass
+			if not self.description: self.description=self.soup.find("div", {"class": "clr lheight20 large"})   #for olx
+			if self.description is None: 
+				print ("Did  not find any description on the offer page:")
+				print(self.link)
+				continue
 			
 			self.output_partial=[]
 			hitcount=0
 			for self.word in self.words:					
-				if re.search(self.word,self.soup.text):
+				if re.findall(self.word,self.description.text):
 					hitcount+=1
-					self.output_partial.append(re.findall(".{25}"+self.word+".{25}",self.soup.text))      #save link and word with chars around it
+					self.output_partial.append(re.findall(".{25}"+self.word+".{25}",self.description.text))      #save link and word with chars around it
 					
-			if and_or=="or": self.output.update({self.link:self.output_partial})
+			if and_or=="or" and hitcount : self.output.update({self.link:self.output_partial})
 			if and_or=="and" and hitcount==len(self.words): self.output.update({self.link:self.output_partial})
 			
 		print("Done.")
 		return self.output
+		
+	#returns list of prices for one page (bs4 object is passed as argument)	
+	def _get_prices_from_page(self,soup):
+		self.prices=[]
+		self.soup=soup
+		self.righttds = self.soup.find_all("td", {"class": "wwnormal tright td-price"})
+
+		for self.entry in self.righttds:
+			if "promoted-list" in str(self.entry.find_parent('table')):
+				continue   										#skip promoted offers (they double)
+
+			if "<strong>" not in str(self.entry) : continue   #because there are empty entries 
+			for self.price in self.entry:
+				
+				try:
+					self.price_int=self.price.find('strong').get_text()
+					if ',' in self.price_int: self.price_int=self.price_int.split(',')[0] 	#sometimes price has comma
+					self.price_int=re.sub('\D','',self.price_int)  				#remove everything except digits
+					self.prices.append(int(self.price_int))
+				except: pass	
+		return self.prices
+	#uses _get_prices_from_page for every page
+	def get_all_prices(self):
+		self.all_prices=[]
+		self.sum=0
+		self.count=0
+		for self.page_nr in range(1,self.nr_of_pages+1):
+			if self.url[-1:] is "/": self.r  = requests.get(self.url+"?page="+str(self.page_nr))  #because there are different versions of links, I want to move this to init
+			else:                    self.r  = requests.get(self.url+"&page="+str(self.page_nr))
+
+			self.data = self.r.text
+			self.soup = BeautifulSoup(self.data,features="html.parser")
+			self.page_prices=self._get_prices_from_page(self.soup)
+			for self_price in self.page_prices: 
+				self.all_prices.append(self_price)
+				self.sum+=self_price
+				self.count+=1
+
+			self.progress.run()
+
+		return {'prices': self.all_prices,
+				'count': self.count,
+				'average': self.sum//self.count}
+'''
+#TEST		
+url="https://www.olx.pl/nieruchomosci/mieszkania/bydgoszcz/?search%5Bfilter_float_price%3Afrom%5D=280000&search%5Bfilter_float_price%3Ato%5D=330000&search%5Bfilter_enum_floor_select%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_select%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_select%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_select%5D%5B3%5D=floor_4&search%5Bfilter_enum_floor_select%5D%5B4%5D=floor_5&search%5Bfilter_enum_floor_select%5D%5B5%5D=floor_6&search%5Bfilter_enum_floor_select%5D%5B6%5D=floor_7&search%5Bfilter_enum_floor_select%5D%5B7%5D=floor_8&search%5Bfilter_enum_floor_select%5D%5B8%5D=floor_9&search%5Bfilter_enum_floor_select%5D%5B9%5D=floor_10&search%5Bfilter_enum_floor_select%5D%5B10%5D=floor_11&search%5Bfilter_enum_builttype%5D%5B0%5D=blok&search%5Bfilter_enum_builttype%5D%5B1%5D=apartamentowiec&search%5Bfilter_float_m%3Afrom%5D=45&search%5Bfilter_float_m%3Ato%5D=60&search%5Bfilter_enum_rooms%5D%5B0%5D=two&search%5Bfilter_enum_rooms%5D%5B1%5D=three"
+answ=OLXJJ(url).get_all_prices()
+for x in answ:
+	print(x+" : "+str(answ.get(x)))
+'''
